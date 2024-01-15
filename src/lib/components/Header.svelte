87bfccc1eccb3aee1ed594f02e016e427d31531f
Max Lee <maxjerry0107@gmail.com>
2024-01-15 04:57:19 -0500

    theme change implemented

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { buttonClass, buttonSizes, buttonVariants } from '$lib/constants';
+	import { cn } from '$lib/utils';
+	import { GithubBrand } from 'svelte-awesome-icons';
+	import SearchCityDialog from './SearchCityDialog.svelte';
+	import ThemeSwitcher from './ThemeSwitcher.svelte';
+</script>
+
+<nav class="flex w-full items-center justify-between py-4">
+	<div />
+	<div class="flex w-full gap-2 sm:w-fit">
+		<SearchCityDialog />
+		<ThemeSwitcher />
+		<a
+			aria-label="Support project"
+			href={'https://github.com/maxjerry0107/sveltekit-weather-app'}
+			class="shrink-0"
+		>
+			<button class={cn(buttonClass, buttonVariants.default, buttonSizes.default, 'h-9')}>
+				<GithubBrand class="h-4 w-4 md:mr-1" />
+				<span class="hidden md:block">Support Project</span>
+			</button>
+		</a>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,25 @@
-<script>
-  import "../app.css";
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import { CodeSolid, HeartRegular } from 'svelte-awesome-icons';
+	import SvelteTheme from 'svelte-themes/SvelteTheme.svelte';
+	import '../app.css';
 </script>
 
-<slot />
+<SvelteTheme attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange />
+
+<Header />
+<main class="flex-grow">
+	<slot />
+</main>
+
+<footer class="py-4">
+	<div
+		class="group flex items-center justify-center gap-1 text-neutral-400 dark:text-neutral-600"
+		aria-hidden={true}
+	>
+		<CodeSolid class="h-5 w-5" />
+		<span>by</span>
+		<HeartRegular class="h-4 w-4 group-hover:text-red-500" />
+		<span>Max</span>
+	</div>
+</footer>
