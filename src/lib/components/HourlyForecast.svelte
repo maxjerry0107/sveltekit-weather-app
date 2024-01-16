dfc3425020494b5ff15a4396d23a208ef1550d35
Max Lee <maxjerry0107@gmail.com>
2024-01-16 09:36:04 -0500

    hourly forecast card

---
+++ src/lib/components/HourlyForecast.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+  export let hourlyData:HourWeather[] = [];
+  </script>
+
+
+<div class="relative rounded-xl border bg-card text-card-foreground shadow-sm md:p-6 order-first col-span-2 flex h-48 cursor-grab touch-auto touch-pan-x select-none scroll-px-0.5 flex-row items-center justify-between gap-12 overflow-hidden overscroll-contain scroll-smooth p-6 ring-offset-background transition-colors scrollbar-hide hover:overflow-x-auto focus:scroll-auto focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 md:order-2 lg:order-3">
+      </div>
